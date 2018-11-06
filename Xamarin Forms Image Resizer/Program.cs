797414fff5981e8d2693e39e6f8765f874b3d684
frank <frank.haugen@gmail.com>
2018-11-06 13:00:34 +0100

    Created project

---
+++ Xamarin Forms Image Resizer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin_Forms_Image_Resizer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
